9fbdd97508977dd87ba5c9ed63ae8f87c7ebb9de
fangyuan <fangyuan4516@gmail.com>
2020-02-10 08:49:21 +0800

    deviceinit 时mapp 强制判断

--- src/mixins/sdkApi.ts
+++ src/mixins/sdkApi.ts
@@ -13,7 +13,7 @@ export default class SdkApi extends Vue {
    const { channel } = sdkOptions;
    origin = origin ? origin : [190, '190'].indexOf(channel) > -1 ? 'u9' : 'api';
    const openid = Cookies.get('openid') || '';
-   const brand = !!openid ? '公众号' : query.mapp ? 'App' : 'web';
+   const brand = !!openid ? '公众号' : !!query.mapp ? 'App' : 'web';
    const devices = getStorage('device') || {};
    let imei = devices.imei || query.imei || query.idfa || openid || '';
    if (!imei && isAndroid && window.android) {
